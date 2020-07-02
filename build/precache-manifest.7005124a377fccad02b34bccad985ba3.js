3b23218dd553bdf8208d03f5d51891c47b55270b
Michael MacLeod <michaelmmacleod@gmail.com>
2020-07-02 11:13:35 -0700

    update build

---
+++ build/precache-manifest.7005124a377fccad02b34bccad985ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd144424018a8c932c2a2a12e536804",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c407e4e76db489548ca7",
+    "url": "./static/css/2.098aa1ea.chunk.css"
+  },
+  {
+    "revision": "33a677e5a6b0294d8a49",
+    "url": "./static/css/main.a0a92219.chunk.css"
+  },
+  {
+    "revision": "c407e4e76db489548ca7",
+    "url": "./static/js/2.874b8748.chunk.js"
+  },
+  {
+    "revision": "33a677e5a6b0294d8a49",
+    "url": "./static/js/main.0a8b388e.chunk.js"
+  },
+  {
+    "revision": "3838457d4350e70f8660",
+    "url": "./static/js/runtime~main.a4a9684c.js"
+  }
+]);
