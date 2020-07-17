d2d389abb899b2ef2b7abfb70941e6c0e68584d6
Michael MacLeod <michaelmmacleod@gmail.com>
2020-07-17 14:08:01 -0700

    add newest build files

---
+++ build/precache-manifest.1d3ed664fad9297b4df908b9b66a8d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6072a075d125b66d9ff588c50e88c6a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d80d2bac81e6f7d8b75",
+    "url": "./static/css/2.155e1274.chunk.css"
+  },
+  {
+    "revision": "75fdb71334a197637de7",
+    "url": "./static/css/main.e21e54bf.chunk.css"
+  },
+  {
+    "revision": "5d80d2bac81e6f7d8b75",
+    "url": "./static/js/2.a1439b53.chunk.js"
+  },
+  {
+    "revision": "75fdb71334a197637de7",
+    "url": "./static/js/main.48f96af2.chunk.js"
+  },
+  {
+    "revision": "3838457d4350e70f8660",
+    "url": "./static/js/runtime~main.a4a9684c.js"
+  }
+]);
