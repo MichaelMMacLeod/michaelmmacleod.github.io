455f0b9c02049a02ce0be2b143cd2c2c19f29862
Michael MacLeod <michaelmmacleod@gmail.com>
2020-07-17 23:40:32 -0700

    Add development instance of the chunk editor
    Stable: build/
    Development: build-devel/

---
+++ build-devel/precache-manifest.37b1423ccad71fea19b08e309a771b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "398f787ce002839673230ae13912558f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5d80d2bac81e6f7d8b75",
+    "url": "./static/css/2.155e1274.chunk.css"
+  },
+  {
+    "revision": "5555a78f34dd145a727a",
+    "url": "./static/css/main.e21e54bf.chunk.css"
+  },
+  {
+    "revision": "5d80d2bac81e6f7d8b75",
+    "url": "./static/js/2.a1439b53.chunk.js"
+  },
+  {
+    "revision": "5555a78f34dd145a727a",
+    "url": "./static/js/main.60d4d421.chunk.js"
+  },
+  {
+    "revision": "3838457d4350e70f8660",
+    "url": "./static/js/runtime~main.a4a9684c.js"
+  }
+]);
