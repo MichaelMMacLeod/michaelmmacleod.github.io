922030b14eb47fe7f21517162c0c940c1ce8d247
Michael MacLeod <michaelmmacleod@gmail.com>
2021-03-17 14:14:49 -0700

    Restart

--- update-kakoi-doc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-set -ex
-
-cd ~/projects/kakoi/documentation
-make compile
-cd ~/projects/michaelmmacleod.github.io
-mv kakoi-documentation old-kakoi-documentation
-cp -r ~/projects/kakoi/documentation/build kakoi-documentation
